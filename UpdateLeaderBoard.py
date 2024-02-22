3581044cd259c867452ad37022f7560f9992f1b6
Akankshabairagi55 <Akankshabairagi55@gmail.com>
2024-02-22 15:56:11 +0530

    Update  leaderboard

---
+++ UpdateLeaderBoard.py
@@ -0,0 +1,29 @@
+import random
+import time
+import json
+
+import os
+os.system("color")
+from termcolor import colored
+
+
+def UpdateLeaderBoard(username,wpm):
+    
+    #json--python dict
+    # with open("LeaderBoard.json","r") as f:
+    #     leaderboard = json.load(f)
+    try:
+        with open("LeaderBoard.json","r") as f:
+            leaderboard = json.load(f)
+    
+    except FileNotFoundError: #To handle the Error of empty file
+        leaderboard = {}
+    
+    leaderboard[username] = wpm
+    
+    #sort
+    leaderboard = dict(sorted(leaderboard.items(),key = lambda item:item[1] ,reverse=True) )
+
+    #python-dict ==>json
+    with open ("LeaderBoard.json","w") as g:
+        json.dump(leaderboard,g)
